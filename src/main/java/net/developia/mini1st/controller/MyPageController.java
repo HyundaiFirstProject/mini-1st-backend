126273489e9aebf0485c0a4261a7693d27a4e2ef
wonsun <98735772+ws1811@users.noreply.github.com>
2023-12-06 11:29:39 +0900

    MyPageController 추가

--- src/main/java/net/developia/mini1st/controller/MyPageController.java
+++ src/main/java/net/developia/mini1st/controller/MyPageController.java
@@ -6,6 +6,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.java.Log;
@@ -13,17 +14,22 @@ import net.developia.mini1st.domain.UserDTO;
 import net.developia.mini1st.service.MyPageService;
 
 @RestController
+@RequestMapping("/api")
 @Log
 public class MyPageController {
 	
 	@Autowired
 	private MyPageService service;
 	
-	@GetMapping(value="/api/getUserInfo/{userno}",
+	@GetMapping(value="/getUserInfo/{userno}",
 				produces= {MediaType.APPLICATION_JSON_VALUE})
 	public ResponseEntity<UserDTO> getUserInfo(@PathVariable("userno") int user_no){
 		log.info("get user info : " + user_no);
-		UserDTO dto = service.getUserInfo(user_no);
+		try {
+			UserDTO dto = service.getUserInfo(user_no);
+		}catch(Exception e) {
+			log.info(e.getMessage());
+		}
 		log.info("user => " + dto.toString());
 		return new ResponseEntity<UserDTO>(dto, HttpStatus.OK);
 	}

a252d015b8db6c0dc054beaea95bf899ba501695
Choi-Seong-Hyeok <cinemay3@daum.net>
2023-12-06 10:34:44 +0900

    feat : 회원가입

---
+++ src/main/java/net/developia/mini1st/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package net.developia.mini1st.mapper;
+
+
+import net.developia.mini1st.domain.UserDTO;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+
+@Mapper
+public interface UserMapper {
+    @Insert("INSERT INTO users (email, password, nickname) VALUES (#{email}, #{password}, #{nickname})")
+    @Options(useGeneratedKeys = true, keyProperty = "user_id")
+    void insertUser(UserDTO userDTO);
+
+
+}

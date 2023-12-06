b62f13895f766c5e6083861fd02c86b208fb61e4
Choi-Seong-Hyeok <cinemay3@daum.net>
2023-12-07 01:40:35 +0900

    feat : checknickname

--- src/main/java/net/developia/mini1st/mapper/UserMapper.java
+++ src/main/java/net/developia/mini1st/mapper/UserMapper.java
@@ -5,12 +5,16 @@ import net.developia.mini1st.domain.UserDTO;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("INSERT INTO users (email, password, nickname) VALUES (#{email}, #{password}, #{nickname})")
     @Options(useGeneratedKeys = true, keyProperty = "user_id")
     void insertUser(UserDTO userDTO);
+
+    @Select("SELECT COUNT(*) FROM users WHERE nickname = #{nickname}")
+    int countUsersByNickname(String nickname);
 
 
 }

c0d4287c48b4cc6a3ab01599dad6522100854594
kimeunseo58 <alan0221@naver.com>
2023-12-07 09:04:44 +0900

    Email Send and get Best Reviews

--- src/main/java/net/developia/mini1st/controller/ReviewController.java
+++ src/main/java/net/developia/mini1st/controller/ReviewController.java
@@ -1,13 +1,44 @@
 package net.developia.mini1st.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
+import net.developia.mini1st.service.ReviewService;
 
 @RestController
 @Slf4j
+@RequestMapping("/api")
 public class ReviewController {
+	
+	private final ReviewService reviewService;
+	
+	@Autowired
+	public ReviewController(ReviewService reviewService) {
+		this.reviewService = reviewService;
+	}
+	
+	@GetMapping("/bestReviews")
+	public ResponseEntity<Map<String, Object>> getBestReviews(){
+		Map<String, Object> response = new HashMap<>();
+		try {
+			response.put("status", "200");
+			response.put("description", "대표 후기 게시물");
+			response.put("data", reviewService.getBestReview());
+			return ResponseEntity.ok(response);
+		} catch (Exception e) {
+            response.put("status", "500");
+			response.put("description", "Internal Server Error");
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+		}
+	}
 	
 	
 }
--- src/main/java/net/developia/mini1st/controller/UserController.java
+++ src/main/java/net/developia/mini1st/controller/UserController.java
@@ -1,7 +1,9 @@
 package net.developia.mini1st.controller;
 
 import lombok.extern.java.Log;
+import net.developia.mini1st.domain.EmailDTO;
 import net.developia.mini1st.domain.UserDTO;
+import net.developia.mini1st.service.EmailSendService;
 import net.developia.mini1st.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,28 +22,46 @@ import java.util.Map;
 public class UserController {
 
     private final UserService userService;
-
+    private final EmailSendService emailSendService;
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, EmailSendService emailSendService) {
         this.userService = userService;
+        this.emailSendService = emailSendService;
     }
 
     @PostMapping("/signup")
     public ResponseEntity<Map<String, String>> signUp(@RequestBody UserDTO userDTO) {
         try {
             userService.signUp(userDTO);
-            // 회원가입 성공 시
+            // �쉶�썝媛��엯 �꽦怨� �떆
             Map<String, String> response = new HashMap<>();
             response.put("status", "200");
-            response.put("description", "회원가입 성공");
+            response.put("description", "�쉶�썝媛��엯 �꽦怨�");
             return ResponseEntity.ok(response);
         } catch (Exception e) {
-            // 회원가입 실패 시
+            // �쉶�썝媛��엯 �떎�뙣 �떆
             log.info(e.getMessage());
             Map<String, String> response = new HashMap<>();
             response.put("status", "422");
-            response.put("description", "회원가입 실패");
+            response.put("description", "�쉶�썝媛��엯 �떎�뙣");
             return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(response);
         }
     }
+    
+    @PostMapping("/sendEmail")
+    public ResponseEntity<Map<String, String>> sendEmail(@RequestBody EmailDTO emailDTO) {
+        Map<String, String> response = new HashMap<>();
+    	try {
+    		emailSendService.sendSimpleMessage(emailDTO.getEmail(), emailDTO.getRandNum());
+    		response.put("status", "200");
+            response.put("description", "이메일 전송 선공");
+            return ResponseEntity.ok(response);
+    	}
+    	catch (Exception e){
+    		response.put("status", "500");
+			response.put("description", "Internal Server Error");
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    	}
+    }
+    
 }
